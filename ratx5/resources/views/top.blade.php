73ebb1dec6767e0965e4ce1e57d091d102d88d0d
Masaru <hittec9609@icloud.com>
2022-05-30 16:02:22 +0900

    organize, flowchartpage

--- ratx5/resources/views/top.blade.php
+++ ratx5/resources/views/top.blade.php
@@ -1,14 +1,13 @@
 @include("layouts.head")
-@include("layouts.header.main_header")
 
 <div class="page_wrapper">
     <div class="message_wrapper">
-        <h1 class="welcome_message">chartmanへようこそ</h1>
+        <h1>chartmanへようこそ</h1>
     </div>
     <div class="login_wrapper">
         <div class="login_box">
-            <a type="button" class="btn btn-outline-success login_register_btn" href="{{ url('/login') }}"><i class="bi bi-arrow-up-right-square-fill" style="font-size: 1.5rem"></i> &ensp;ログイン</a>
-            <a type="button" class="btn btn-outline-primary  login_register_btn" href="{{ url('/register') }}"><i class="bi bi-arrow-down-right-square-fill" style="font-size: 1.5rem"></i> &ensp;新規登録</a>
+            <a type="button" class="btn btn-success login_register_btn" href="{{ url('/login') }}"><i class="bi bi-arrow-up-right-square" style="font-size: 1.5rem"></i> &ensp;ログイン</a>
+            <a type="button" class="btn btn-primary  login_register_btn" href="{{ url('/register') }}"><i class="bi bi-arrow-down-right-square" style="font-size: 1.5rem"></i> &ensp;新規登録</a>
 
 
         </div>
