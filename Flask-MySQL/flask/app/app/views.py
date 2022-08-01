d294549bd17c85dd1d00352efbafdc831dd37379
Masaru <hittec9609@icloud.com>
2022-08-02 07:25:58 +0900

    AppBuilder

--- Flask-MySQL/flask/app/app/views.py
+++ Flask-MySQL/flask/app/app/views.py
@@ -7,17 +7,24 @@ from . import appbuilder, db
 
 class MyView(BaseView):
 
-    default_view = 'method1'
+    route_base = "/myview"
 
-    @expose('/method1/')
-    @has_access
-    def method1(self):
+    @expose('/method1/<string:param1>')
+
+    def method1(self, param1):
         # do something with param1
         # and return to previous page or index
 
-        return 'Hello'
+        return param1
+
+    @expose('/method2/<string:param1>')
+    def method2(self, param1):
+        # do something with param1
+        # and render it
+        param1 = 'Hello %s' % (param1)
+        return param1
 
 
-appbuilder.add_view(MyView, "Method1", category='My View')
+appbuilder.add_view_no_menu(MyView())
 
 db.create_all()
