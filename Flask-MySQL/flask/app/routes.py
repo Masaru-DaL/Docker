96301c7a8e48992571fc060c8ea3a859a1b713b9
Masaru <hittec9609@icloud.com>
2022-08-08 12:21:50 +0900

    todo

--- Flask-MySQL/flask/app/routes.py
+++ Flask-MySQL/flask/app/routes.py
@@ -1,7 +1,42 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from app import app
 from app import database as db_helper
 
+@app.route("/delete/<int:task_id>", method=['POST'])
+def delete(task_id):
+  try:
+    result = {'success': True, 'response': 'Removed task'}
+  except:
+    result = {'success': False, 'response': 'Something went wrong'}
+
+  return jsonify(result)
+
+
+@app.route("/edit/<int:task_id>", method=['POST'])
+def update(task_id):
+  data = request.get_json()
+    print(data)
+  try:
+    if "status" in data:
+      result = {'success': True, 'response': 'Status Updated'}
+    elif "description" in data:
+      result = {'success': True, 'response': 'Task Updated'}
+    else:
+      result = {'success': True, 'response': 'Nothing Updated'}
+  except:
+    result = {'success': False, 'response': Something went wrong}
+
+  return jsonify(result)
+
+
+@app.route("/create", method=['POST'])
+def create():
+  data = request.get_json()
+  result = {'success': True, 'response': 'Done'}
+  return jsonify(result)
+
+
 @app.route("/")
 def homepage():
-	return render_template("index.html", name=db_helper())
+  items = db_helper.fetch_todo
+	return render_template("index.html", items=items)
---
+++ Flask-MySQL/flask/app/static/modal.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('#task-modal').on('show.bs.modal', function (event) {
+    const button = $(event.relatedTarget);
+
+  })
+})
