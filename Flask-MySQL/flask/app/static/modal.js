441c055cda1944c9e6293db88adc301c054d4a86
Masaru <hittec9609@icloud.com>
2022-08-09 11:13:47 +0900

    todo

--- Flask-MySQL/flask/app/static/modal.js
+++ Flask-MySQL/flask/app/static/modal.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
   $('#task-modal').on('show.bs.modal', function (event) {
     const button = $(event.relatedTarget);
+    const taskID = button.data('source');
+    const content = button.data('content');
 
+    const modal = $(this);
+
+    if (taskID === 'New Task') {
+      modal.find('.modal-title').text(taskID);
+      $('#task-form-display').removeAttr('taskID');
+    } else {
+      modal.find('.modal-title').text('Edit Task ' + taskID);
+      $('#task-form-display').attr('taskID', taskID);
+    }
+
+    if (content) {
+      modal.find('.form-control').val(content);
+    } else {
+      modal.find('.form-control').val('');
+    }
   })
 })
