d081881c0adf68279d2280c103bf8dbc7824ba47
Masaru <hittec9609@icloud.com>
2022-08-09 11:46:25 +0900

    todo

--- Flask-MySQL/flask/app/static/modal.js
+++ Flask-MySQL/flask/app/static/modal.js
@@ -20,4 +20,68 @@ $(document).ready(function () {
       modal.find('.form-control').val('');
     }
   })
-})
+
+  $('#submit-task').click(function (){
+    const tID = $('#task-form-display').attr('taskID');
+    console.log($('#task-modal').find('.form-control').val());
+    $.ajax({
+      type: 'POST',
+      url: tID ? '/edit' + tID : '/create',
+      contentType: 'application/json;charset=UTF-8',
+      data: JSON.stringify({
+        'description': $('#task-modal').find('.form-control').val()
+      }),
+      success: function (res) {
+        console.log(res.response)
+        location.reload();
+      },
+      error:function () {
+        console.log('Error');
+      }
+    });
+  });
+
+  $('.remove').click(function () {
+    const remove = $(this);
+    $.ajax({
+      type: 'POST',
+      url: '/delete' + remove.data('source'),
+      success: function (res) {
+        console.log(res.response)
+        location.reload();
+      },
+      error: function () {
+        console.log('Error');
+      }
+    });
+  });
+
+  $('.state').click(function () {
+    const state = $(this);
+    const tID = state.data('source');
+    const new_state;
+    if (state.text() === "In Progress") {
+      new_state = "Complete";
+    } else if (state.text() === "Complete") {
+      new_state = "Todo";
+    } else if (state.text() === "Todo") {
+      new_state = "In Progress"
+    }
+
+    $.ajax({
+      type: 'POST',
+      url: '/edit' + tID,
+      contentType: 'application/json;charset=UTF-8',
+      data: JSON.stringify({
+        'status': new_state
+      }),
+      success: function (res) {
+        console.log(res)
+        location.reload();
+      },
+      error: function () {
+        console.log('Error');
+      }
+    });
+  });
+});
