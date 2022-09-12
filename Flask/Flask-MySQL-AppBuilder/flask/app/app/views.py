d46662c25c45f81a237a46194b3d77401cd15aeb
Masaru <hittec9609@icloud.com>
2022-09-12 14:09:02 +0900

    Organize

---
+++ Flask/Flask-MySQL-AppBuilder/flask/app/app/views.py
@@ -0,0 +1,22 @@
+from flask import flash
+from flask_appbuilder import SimpleFormView
+from flask_babel import lazy_gettext as _
+
+from . import appbuilder, db
+from .form import MyForm
+
+
+class MyFormView(SimpleFormView):
+    form = MyForm
+    form_title = 'This is my first form view'
+    message = 'My form submitted'
+
+    def form_get(self, form):
+        form.field1.data = 'This was prefilled'
+
+    def form_post(self, form):
+        # post process form
+        flash(self.message, 'info')
+
+appbuilder.add_view(MyFormView, "My form View", icon="fa-group", label=_('My form View'),
+                     category="My Forms", category_icon="fa-cogs")
